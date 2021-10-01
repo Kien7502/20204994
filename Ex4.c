7130e3d6c9bbd023de8c6662ab27de685a00a8a4
Kien7502 <91709919+Kien7502@users.noreply.github.com>
2021-10-01 11:18:48 +0700

    Update Ex4.c

--- Ex4.c
+++ Ex4.c
@@ -1,14 +1,10 @@
-void split(double num, int *int_part,double *frac_part){
-  *int_part = (int)num;
-  *frac_part = num -*int_part;
-}
-int main(void){
+#include <stdio.h>
+int main(){
   double num, fraction;
   int integer;
   printf("Please  enter a real number: ");
   scanf("%f", &num);
-  split(num, &integer, &fraction);
-  printf("The integer part is %d\n", integer);
-  printf("The remaining fraction is %f\n", fraction);
-  return0;
+  printf("The integer part is %d\n", (int)num);
+  printf("The remaining fraction is %f\n", num -(int)num);
+  return 0;
 }
